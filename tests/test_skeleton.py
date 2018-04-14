27d17bacbac1069825ef2103097d44e2fdac71e3
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2018-04-14 13:55:23 +0200

    Initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from haki_prawojazdycompl.skeleton import fib
+
+__author__ = "Jonatan Witoszek"
+__copyright__ = "Jonatan Witoszek"
+__license__ = "mit"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
